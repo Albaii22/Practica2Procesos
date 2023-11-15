179969928f8a7e42f8c1cc094ef2caf748f8daa2
Alumno <usr3584@salesianos-lacuesta.net>
2023-11-15 19:14:11 +0100

    feat: Creacion del experimento tres

--- src/net/salesianos/Tienda.java
+++ src/net/salesianos/Tienda.java
@@ -4,17 +4,16 @@ public class Tienda {
     public static void main(String[] args) {
         Huerto huerto = new Huerto(5);
 
-        Thread granjeroPaco = new Thread(new Granjero("Paco", huerto, 10));
-        Thread granjeroRamon = new Thread(new Granjero("Ramón", huerto, 10));
+        Thread granjeroPaco = new Thread(new Granjero("Paco", huerto, 5));
+        granjeroPaco.setPriority(Thread.MAX_PRIORITY);
 
-        Thread clienteFaustino = new Thread(new Cliente("Faustino", huerto, 5));
-        Thread clienteGentleman = new Thread(new Cliente("Mr.Gentleman", huerto, 10));
-        Thread clienteLoquendo = new Thread(new Cliente("Loquendo", huerto, 5));
+        Thread granjeroJose = new Thread(new Granjero("Jose", huerto, 5));
+        granjeroJose.setPriority(Thread.MAX_PRIORITY);
+
+        Thread cliente = new Thread(new Cliente("Adhernea", huerto, 5));
 
         granjeroPaco.start();
-        granjeroRamon.start();
-        clienteFaustino.start();
-        clienteGentleman.start();
-        clienteLoquendo.start();
+        granjeroJose.start();
+        cliente.start();
     }
 }
