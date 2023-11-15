f7b2593e0d086e5a4141ac1ca4ab9c7711c36cbe
Alumno <usr3584@salesianos-lacuesta.net>
2023-11-15 18:59:11 +0100

    feat: Creacion del ejercicio sin los experimentos

--- src/net/salesianos/Tienda.java
+++ src/net/salesianos/Tienda.java
@@ -4,10 +4,17 @@ public class Tienda {
     public static void main(String[] args) {
         Huerto huerto = new Huerto(5);
 
-        Thread granjero = new Thread(new Granjero("Paco", huerto, 5));
-        Thread cliente = new Thread(new Cliente("Jose", huerto, 5));
+        Thread granjeroPaco = new Thread(new Granjero("Paco", huerto, 10));
+        Thread granjeroRamon = new Thread(new Granjero("Ramón", huerto, 10));
 
-        granjero.start();
-        cliente.start();
+        Thread clienteFaustino = new Thread(new Cliente("Faustino", huerto, 5));
+        Thread clienteGentleman = new Thread(new Cliente("Mr.Gentleman", huerto, 10));
+        Thread clienteLoquendo = new Thread(new Cliente("Loquendo", huerto, 5));
+
+        granjeroPaco.start();
+        granjeroRamon.start();
+        clienteFaustino.start();
+        clienteGentleman.start();
+        clienteLoquendo.start();
     }
 }
